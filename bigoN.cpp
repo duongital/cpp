4948d09fbd89d4bbea70986fb823a5d1203d0ceb
duongital <duongital@gmail.com>
2024-03-15 11:26:56 +0700

    chore: sliding window

---
+++ bigoN.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+vector<int> productExceptSelf(vector<int> &nums)
+{
+    // all product
+    int all = 1;
+    int countZero = 0;
+    for (int i : nums)
+    {
+        int multiplyBy = i == 0 ? 1 : i;
+        all *= multiplyBy;
+        if (i == 0)
+        {
+            countZero++;
+        }
+    }
+
+    vector<int> allZero(nums.size(), 0);
+
+    if (countZero == 0)
+    {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            allZero[i] = all / nums[i];
+        }
+    }
+    else if (countZero == 1)
+    {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == 0)
+                allZero[i] = all;
+        }
+    }
+    return allZero;
+}
+
+int main()
+{
+
+    vector<int> test = {1,2,3,4,5};
+    vector<int> test2 = productExceptSelf(test);
+    // cout << test << endl;
+    // return 0;
+    cout << "hello" << endl;
+    for (int i : test2)
+    {
+        cout << i << endl;
+    }
+    return 0;
+}
---
+++ slidingWindow.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int slidingWindowAtMost(vector<int> &nums, int goal)
+{
+    int start = 0, currentSum = 0, totalCount = 0;
+
+    // Iterate through the array using a sliding window approach
+    for (int end = 0; end < nums.size(); end++)
+    {
+        currentSum += nums[end];
+
+        // Adjust the window by moving the start pointer to the right
+        // until the sum becomes less than or equal to the goal
+        while (start <= end && currentSum > goal)
+        {
+            currentSum -= nums[start++];
+        }
+
+        // Update the total count by adding the length of the current subarray
+        totalCount += end - start + 1;
+    }
+    return totalCount;
+}
+
+int numSubarraysWithSum(vector<int> &nums, int goal)
+{
+    return slidingWindowAtMost(nums, goal) - slidingWindowAtMost(nums, goal - 1);
+}
+
+int main()
+{
+    vector<int> test = {1, 0, 1, 0, 1};
+    int x = numSubarraysWithSum(test, 2);
+    cout << x << endl;
+
+    return 0;
+}
